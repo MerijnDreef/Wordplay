ecf22e574dc550630bbaf13b4808141225068cba
Merijn <99054535@mydavinci.nl>
2022-11-23 09:52:16 +0100

    added the Vue front-end

--- resources/js/components/loginVue.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>login Vue!</h1>
-</template>
-
-<script>
-export default {
-    name: 'loginVue'
-}
-</script>
--- resources/js/components/registerVue.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>register Vue!</h1>
-</template>
-
-<script>
-export default {
-    name: 'registerVue'
-}
-</script>
