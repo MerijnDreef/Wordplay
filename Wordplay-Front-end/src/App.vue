35a784f30d82b3f172c84093417d9350b2684f8d
Merijn <99054535@mydavinci.nl>
2022-11-25 13:38:20 +0100

    added axios and made sure to get stuff from the back end

--- Wordplay-Front-end/src/App.vue
+++ Wordplay-Front-end/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+// import axios from 'axios';
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
--- Wordplay-Front-end/src/views/AboutView.vue
+++ Wordplay-Front-end/src/views/AboutView.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h3>{{ posts.message }}</h3>
   </div>
 </template>
 
+<script lang="ts">
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    axios('http://127.0.0.1:8000/api/test')
+      .then((response) => {
+        this.posts = response.data
+      })
+  }
+}
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
